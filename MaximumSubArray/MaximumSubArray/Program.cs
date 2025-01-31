1bec2b07bd8f8e00220756a9098c32a6b5c81365
PasinduJayasinghe <thisarajayasinghe07@gmail.com>
2025-02-01 04:29:23 +0530

    MaximumSubArray

---
+++ MaximumSubArray/MaximumSubArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MaximumSubArray
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxSubArray(new int[] { 2, -3, 5, -4, 6, 9, -2 }));
+        }
+
+        public static int MaxSubArray(int[] nums)
+        {
+            int maxSub = nums[0];
+            int currentSum = 0;
+
+            foreach (int x in nums) {
+                if (currentSum < 0) {
+                    currentSum = 0;
+                    
+
+                }
+                currentSum = currentSum + x;
+                maxSub = Math.Max(maxSub, currentSum);
+            }
+
+            return maxSub;
+        }
+    }
+}
